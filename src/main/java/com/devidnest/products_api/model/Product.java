749ac8293dda4f13bdaab8d470fb1ef468c44f46
David Nest <davidcamposoliveira80@gmail.com>
2026-05-13 10:13:07 -0300

    Inserting Annotations(@) in the class Product

--- src/main/java/com/devidnest/products_api/model/Product.java
+++ src/main/java/com/devidnest/products_api/model/Product.java
@@ -1,5 +1,6 @@
 package com.devidnest.products_api.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,11 +8,22 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "product")
 public class Product {
 
-    private String id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "description")
     private String description;
+
+    @Column(name = "price")
     private Double price;
 
 }
