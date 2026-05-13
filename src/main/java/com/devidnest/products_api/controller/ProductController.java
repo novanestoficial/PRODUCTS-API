4a45bb7d103ec5fae42dbdc9fe1aacddd5d6dea1
David Nest <davidcamposoliveira80@gmail.com>
2026-05-13 20:18:51 -0300

    Insert method FindById(getProductById) in the System

--- src/main/java/com/devidnest/products_api/controller/ProductController.java
+++ src/main/java/com/devidnest/products_api/controller/ProductController.java
@@ -3,10 +3,7 @@ package com.devidnest.products_api.controller;
 
 import com.devidnest.products_api.model.Product;
 import com.devidnest.products_api.repositorie.ProductRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("products")
@@ -22,5 +19,12 @@ public class ProductController {
     public void saveProduct(@RequestBody Product product) {
         System.out.println("Saving product: " + product);
         productRepository.save(product);
+    }
+
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable Long id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Produto não encontrado"));
     }
 }
